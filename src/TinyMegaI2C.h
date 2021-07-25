60fa2ad4d287258acff2526e50986646844f1620
Saurabh Datta <hi@dattasaurabh.com>
2021-07-25 16:07:52 +0800

    Testing compiler time issue..

--- src/TinyMegaI2C.h
+++ src/TinyMegaI2C.h
@@ -8,30 +8,42 @@
   uC:           ATTINY-1607
 ******************************************************************************/
 
-#pragma once
+// #pragma once
+
+// #ifndef TinyMegaI2CMaster_h
+// #define TinyMegaI2CMaster_h
+
+#ifndef TinyMegaI2C_h
+#define TinyMegaI2C_h
 
 #include <stdint.h>
 #include <Arduino.h>
 #include <avr/io.h>
 
 // 400kHz clock
-uint32_t const FREQUENCY = 400000L;  // Hardware I2C clock in Hz
-uint32_t const T_RISE = 300L;        // Rise time
+uint32_t const FREQUENCY = 400000L; // Hardware I2C clock in Hz
+uint32_t const T_RISE = 300L;       // Rise time
 
-class TinyMegaI2CMaster {
-  public:
-    TinyMegaI2CMaster( void );
+class TinyMegaI2CMaster
+{
+public:
+  // TinyMegaI2CMaster(void);
+  TinyMegaI2CMaster(void);
 
-    void init(void);
+  void init(void);
 
-    uint8_t read(void);
-    uint8_t readLast(void);
-    bool write(uint8_t data);
-    bool start(uint8_t address, int readcount);
-    bool restart(uint8_t address, int readcount);
-    void stop(void);
-  private:
-    int I2Ccount;
+  uint8_t read(void);
+  uint8_t readLast(void);
+
+  bool write(uint8_t data);
+  bool start(uint8_t address, int readcount);
+  bool restart(uint8_t address, int readcount);
+  void stop(void);
+
+private:
+  int I2Ccount;
 };
 
 extern TinyMegaI2CMaster TinyMegaI2C;
+
+#endif
